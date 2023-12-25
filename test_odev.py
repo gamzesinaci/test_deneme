47571674ee0127926267b2f5306b1df570c21bbd
gamze sinaci <gamze-manga.1999@hotmail.com>
2023-12-25 19:00:01 +0300

    first commit

---
+++ test_odev.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from selenium.webdriver.support.wait import WebDriverWait #ilgili driverı bekleten yapı
+from selenium.webdriver.support import expected_conditions as ec #beklenen koşullar
+import pytest
+
+
+class Test:
+    def setup_method(self): #her test başlangıcında çalışacak fonk
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.saucedemo.com")
+        self.driver.maximize_window() 
+
+    def teardown_method(self): # her testinin bitiminde çalışacak fonk
+        self.driver.quit()
+
+    @pytest.mark.parametrize("username,password",[("","")])
+    def test_invalid_login(self,username,password):
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput.send_keys(username)
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        assert errorMessage.text == "Epic sadface: Username is required"
+
+    @pytest.mark.parametrize("username,password",[("locked_out_user","")])    
+    def test_invalid_iki(self,username,password):
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput.send_keys(username)
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        assert errorMessage.text == "Epic sadface: Password is required"
+    
+    @pytest.mark.parametrize("username,password",[("locked_out_user","secret_sauce")])    
+    def test_invalid_uc(self,username,password):
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput.send_keys(username)
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        assert errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
+    @pytest.mark.parametrize("username,password",[("standard_user","secret_sauce")])  
+    def test_valid_login_dort(self,username,password):
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
+        usernameInput.send_keys(username)
+        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.ID,"login-button")
+        loginButton.click()
+        self.driver.execute_script("window.scrollTo(0,500)") 
+        addToCart = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")))
+        addToCart.click()
+        remove = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='remove-test.allthethings()-t-shirt-(red)']")))
+        assert remove.text == "Remove"  
